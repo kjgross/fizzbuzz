a5073453e6875e0b00d7dea945197845086b01f4
Kelsey <kelsey@edmodo.com>
2014-08-27 22:10:49 -0700

    add extrac

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,12 +5,22 @@
 # If neither, print the number
 # V1 hardcode N = 100
 # V2 let user pick N either when running the script or via raw input
-	
+# V3 You should figure out where this user behavior would raise an exception 
+# in your code, use a try/except block to catch it, and then respond by print
+# a message to the user that they need to supply numeric inputs for the program,
+#  then follow that up by using raw_input() to ask for a new numeric value.	
 		
 import sys
 
 if len(sys.argv) == 2:
-	max = int(sys.argv[1])
+	while True:
+		try:
+			sys.argv[1] = int(sys.argv[1])
+			max = int(sys.argv[1])
+			break
+		except ValueError:
+			max = int(raw_input("Not a valid integer! Please try again. "))
+			break
 else: 
 	max = int(raw_input("Please enter an integer: "))
 	
