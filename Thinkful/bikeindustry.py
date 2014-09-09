78c4b67ce86e1cf1e55e17bd69e3c97eafc67fd9
Kelsey <kelsey@edmodo.com>
2014-09-08 20:39:00 -0700

    add new bike files

--- Thinkful/bikeindustry.py
+++ Thinkful/bikeindustry.py
@@ -1,19 +1,16 @@
-#wheel Light, Medium, Heavy
-#Frame Aluminum, Carbon, Steel
-# Each Manufacturer produceses 3 models of bike
 
 from models import *
 
-wheel_light = Wheels("Wheel Light", 50, 100)
-wheel_medium = Wheels("Wheel Medium", 100, 75)
-wheel_heavy = Wheels("Wheel Heavy", 150, 25)
+wheel_light = Wheels("Wheel Light", 50, 10)
+wheel_medium = Wheels("Wheel Medium", 100, 20)
+wheel_heavy = Wheels("Wheel Heavy", 150, 30)
 
-frame_aluminum = Frames("Frame Aluminum", 100, 300)
-frame_carbon = Frames("Frame Carbon", 50, 400)
+frame_aluminum = Frames("Frame Aluminum", 100, 50)
+frame_carbon = Frames("Frame Carbon", 50, 100)
 frame_steel = Frames("Frame Steel", 125, 150)
 
-lightweightbikes = Manufacturer("Light Weight Bikes", 5)
-heavydutybikes = Manufacturer("Heavy Duty Bikes", 7)
+lightweightbikes = Manufacturer("Light Weight Bikes", 1.25)
+heavydutybikes = Manufacturer("Heavy Duty Bikes", 1.25)
 
 modela = BikeModels("Model A", lightweightbikes, wheel_light, frame_aluminum)
 modelb = BikeModels("Model B", lightweightbikes, wheel_light, frame_carbon)
@@ -22,57 +19,107 @@ modeld = BikeModels("Model D", heavydutybikes, wheel_medium, frame_aluminum)
 modele = BikeModels("Model E", heavydutybikes, wheel_heavy, frame_aluminum)
 modelf = BikeModels("Model F", heavydutybikes, wheel_heavy, frame_steel)
 
-bobs_bikes = BikeShop("Bob's Bikes", 20, [modela, modelb, modelc, modeld, modele, modelf])
+bobs_bikes = BikeShop("Bob's Bikes", 1.20, [modela, modelb, modelc, modeld, modele, modelf], 0)
 
-paul = Customer("Paul", 200, [])
-melanie = Customer("Melanie", 500, [])
-rick = Customer("Rick", 1000, [])
+paul = Customer("Paul", 200)
+melanie = Customer("Melanie", 500)
+rick = Customer("Rick", 1000)
 
 inventory = [modela, modelb, modelc, modeld, modele, modelf]
 
-
 def describe_inventory(inventory):
+	"""Print out the current inventory, including weight and cost"""
 	for i in inventory:
-		print "Bob's bikes sells the %s which weighs %s" % (i.name,i.weight())
-	print "that's all."
+		#bobs_bikes.margin_calc(i, bobs_bikes.margin)
+		print "Bob's bikes sells the %s which weighs %s pounds for %s dollars." % (i.name,i.weight(), i.cost())#*bobs_bikes.margin)
 
 def customers_options(inventory, customer):
+	"""Print out a customer's options given their budget"""
+	print "Let's see what %s can buy with %s dollars in their pocket" % (customer.name, customer.budget)
+	customer_model_options = []
 	for i in inventory:
-		if customer.budget >= i.cost:
-			print "Customer %s can purchase bike: %s" % (customer.name, i.model)
+		if customer.budget >= i.cost()*bobs_bikes.margin:
+			customer_model_options.append(i)
 		else:
 			continue
+	print "Customer %s can buy the following bikes:" % (customer.name)
+	describe_inventory(customer_model_options)
+	
 
-# 3 customers buy bikes:
-#buy the bike
 
-# print the remaining inventory .. then move on to the next customer
-
-def purchase_bike(model, customer, inventory):
-	if model in inventory and model.cost < customer.budget:
-		leftoverbudget = customer.budget - model.cost
-		BikeShop.sell_bike(model, model.cost)
+def purchase_bike(model, customer, inventory, bikeshop):
+	"""Purchase the bike for a given customer"""
+	print "Alright, %s would like to buy %s." % (customer.name, model.name)
+	if model in inventory and model.cost() <= customer.budget:
+		customer.budget -= model.cost()
+		bikeshop.profits += model.cost()
+		bobs_bikes.sell_bike(model, model.cost(), customer, inventory, bobs_bikes)
+		print "The following models are left in stock: "
 		describe_inventory(inventory)
 	else:
-		print model.cost
+		print model.cost()
 		print "Nope, %s can't buy that guy" % (customer.name)
 
+# Currently not using this method, but could be fun if I expanded this project.
+def choose():
+	"""Option for end-user to pick who they want to be shopping as"""
+	choice = raw_input("Who would you like to be? Paul, Melanie, or Rick? ")
+	if choice == "Paul":
+		purchase_bike(modela, paul, inventory, bobs_bikes)
+	elif choice == "Melanie":
+		purchase_bike(modela, melanie, inventory, bobs_bikes)
+	elif choice == "Rick":
+		purchase_bike(modela, rick, inventory, bobs_bikes)
+	else:
+		print "Hmm. That person isn't here today, pick again!" 
+		choose()
 
-print "here we go kids"
+def manumarks(manufacturer, mark_up_percent, model):
+	manufacturer.mark_up_cost(mark_up_percent, model)
+	print "OK!"
+
+
+## Begin printing of script 
+
+print "Let's go Bike Shopping"
+print "We're walking into %s store now." % (bobs_bikes.name)
+print "Lot's of bikes are around, namely: "
+describe_inventory(inventory)
+print "Alrighty then, Melanie is up first!"
+customers_options(inventory, melanie)
+purchase_bike(modela, melanie, inventory, bobs_bikes)
+print "Cool. Rick is up second."
+customers_options(inventory, rick)
+purchase_bike(modele, rick, inventory, bobs_bikes)
+print "Nice nice. Paul is last!"
+customers_options(inventory, paul)
+purchase_bike(modelc, paul, inventory, bobs_bikes)
+
+
+print "dooone"
+manumarks(heavydutybikes, 2, modeld)
+
+## Test Code
+#add_manu_markup(inventory, heavydutybikes)
 #describe_inventory(inventory)
-print "ok that's cool"
+#mark_up_cost(heavydutybikes, 2, modela)
+# choose()
+#customers_options(inventory, melanie)
+#customers_options(inventory, paul)
+#customers_options(inventory, rick)
+#purchase_bike(modela, melanie, inventory, bobs_bikes)
+#purchase_bike(modelb, paul, inventory, bobs_bikes)
 
-# print modela.name
-# print modela.wheel1.name
-# print modela.frame.name
-# print modela.manufacturer.name
-print modela.weight
-print modela.cost()
 
-print "lets buy somethin"
-purchase_bike(modela, paul, inventory)
-purchase_bike(modela, rick, inventory)
-print "lalala"
+print "lalala the script is done"
+
+
+## To Do:
+
+# Handle cost so that both manufacturer's and shop's mark ups are included
+	## The bicycle shop should charge its customers 20% over its cost for buying the bikes 
+	## from the manufacturers.
+
 
 
 
--- Thinkful/models.py
+++ Thinkful/models.py
@@ -1,28 +1,20 @@
 # Bicycle Manufacturers
-
-# Have a name
-# Produce three models of bikes each
-# Have a percentage over cost they sell bikes to bike shops at
-# You'll need to create two bicycle manufacturers
-
 class Manufacturer(object):
 	"""Makes different bike models and sells them at a mark-up"""
 	def __init__(self, name, mark_up_percent):
 		self.name = name
 		self.mark_up_percent = mark_up_percent
-
+	def mark_up_cost(self, mark_up_percent, model):
+		print "I'm doing a manu markup"
+		price = model.cost()
+		price *= mark_up_percent
+		print "It's gonna cost you %s" % (price)
+		return price
 
 
 # Bicycle Models
-
-# Comprised (in our simplified world) of two wheels of the same type and a frame.
-# Have a total weight equal to the sum of the weight of the frame and two wheels.
-# Have a total cost to produce (for our purposes, that cost is the sum of the two wheels' and frame's cost to produce)
-# Have a name
-# Have a manufactuer
-
 class BikeModels(object):
-	"""Compiles wheels and frame to make a complete bike object"""
+	"""Compiles wheels and frame to make a complete bike object with a weight and cost of each part"""
 	def __init__(self, name, manufacturer, wheel_type, frame):
 		self.name = name
 		self.manufacturer = manufacturer
@@ -31,26 +23,12 @@ class BikeModels(object):
 		self.frame = frame
 	def weight(self):
 		"""Calculate total weight of the bike"""
-		self.weight = self.wheel1.weight + self.wheel2.weight + self.frame.weight
+		return self.wheel1.weight + self.wheel2.weight + self.frame.weight
 	def cost(self):
 		"""calculate total cost of building the bike"""
-		#print self.wheel1.cost 
-		#print "wheel cost ^^"
-		#print self.frame.cost 
-		#print "frame cost ^^"
-		self.cost = self.wheel1.cost + self.wheel2.cost + self.frame.cost
-		#print self.cost 
-		#print "total cost ^^"
-
-## Why does weight work but cost doesn't? They are identical functions ##
+		return self.wheel1.cost + self.wheel2.cost + self.frame.cost
 
 # Frames
-
-# Can be made of aluminunum, carbon, or steel
-# Have a weight
-# Have a cost for manufacturer to produce
-# Even though you'll create two bicycle manufacturers, assume that all manufacturers use the same three frame types.
-
 class Frames(object):
 	"""Creates frame objects to become part of a bike"""
 	def __init__(self, name, weight, cost):
@@ -58,15 +36,7 @@ class Frames(object):
 		self.weight = weight
 		self.cost = cost
 
-
 # Wheels
-
-# Have a weight
-# Have a cost for manufacturer to produce
-# Have a model name
-# You'll need to create a total of three wheel types
-# Even though you'll create two bicycle manufacturers, assume that all manufacturers use the same three wheel types.
-
 class Wheels(object):
 	"""Creates Wheel objects to become part of a bike"""
 	def __init__(self, name, weight, cost):
@@ -74,47 +44,36 @@ class Wheels(object):
 		self.weight = weight
 		self.cost = cost
 
-
-
 # Bike Shops
-
-# Get their inventory of bikes from manufacturers
-# Sell bicycle models with a margin over price they pay to manufacturer
-# Have a name
-# Have an inventory
-# Can see how much profit they have made on margin from selling bikes.
-
 class BikeShop(object):
-	def __init__(self, name, margin, inventory):
+	def __init__(self, name, margin, inventory, profits):
 		self.name = name
 		self.margin = margin
 		self.inventory = inventory
+		self.profits = profits
+
 	def profit(self):
 		"""Calculate the profit the shop makes on a sale"""
-		profit = margin*number_sold
+		profits = margin*number_sold
 
-	def sell_bike(self, model, price):
+	def sell_bike(self, model, price, customer, inventory, bikeshop):
 		"""Sell a bike and remove it from bikeshop inventory"""
 		if model in self.inventory:
-			del self.invetory[model]
-			print model + " sold."
-			print "Customer %s purchased bike %s and has %s left over" % (customer.name, model.name, leftoverbudget)
+			print "Good news, we've got it. %s sold." % (model.name)
+			print "Customer %s purchased bike %s and has %s dollars left over" % (customer.name, model.name, customer.budget)
+			print "Store %s now has a profit of %s" % (bikeshop.name, bikeshop.profits)
+			inventory.remove(model)
+####### this line works when it's just inventory.remove(i).. but not with "return self.lalal".. why?
 		else:
-			print model + "is unavailable."
+			print " Unfortunately, %s is unavailable." % (model)
 
 
 # Customers
-
-# Have a name
-# Have a fund of money to buy a bike
-# Can buy and own a new bicycle
-
 class Customer(object):
 	"""Customers have a budget and go to the bike store to buy bikes"""
-	def __init__(self, name, budget, can_buy):
+	def __init__(self, name, budget):
 		self.name = name
 		self.budget = budget
-		self.can_buy = can_buy
 
 
 
